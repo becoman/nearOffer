055c37db0dfe512b77be4cfe0beba25ea7b40167
unknown <Miguel@Miguel-PC.(none)>
2012-06-30 12:48:50 +0200

    primera version º

---
+++ gen/beta/beer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package beta.beer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/beta/beer/NearOfferActivity.java
@@ -0,0 +1,13 @@
+package beta.beer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NearOfferActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
