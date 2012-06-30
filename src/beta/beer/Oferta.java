1b952b482fc2cbb29836957725a8b91ef2733fd8
Mannuel Isasa <manoloisasa@gmail.com>
2012-06-30 17:35:00 +0200

    clase oferta sólo con geters y setters

---
+++ src/beta/beer/Oferta.java
@@ -0,0 +1,166 @@
+package beta.beer;
+import java.util.Date;
+
+public class Oferta {
+	
+	private String url;
+	private int id;
+	private int category_id;
+	private String category_name;
+	private Date start_date;
+	private Date end_date;
+	private Date valid_date;
+	private float original_price;
+	private float price;
+	private float discount;
+	private String title;
+	private String description;
+	private String city;
+	private String address;
+	private int phonme;
+	private String bussiness;
+	private Date publish_date;
+	private String keywords;
+	private String partner;
+	private String image;
+	private String favicon;
+	private float latitude;
+	private float longitude;
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public int getCategory_id() {
+		return category_id;
+	}
+	public void setCategory_id(int category_id) {
+		this.category_id = category_id;
+	}
+	public String getCategory_name() {
+		return category_name;
+	}
+	public void setCategory_name(String category_name) {
+		this.category_name = category_name;
+	}
+	public Date getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+	public Date getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+	public Date getValid_date() {
+		return valid_date;
+	}
+	public void setValid_date(Date valid_date) {
+		this.valid_date = valid_date;
+	}
+	public float getOriginal_price() {
+		return original_price;
+	}
+	public void setOriginal_price(float original_price) {
+		this.original_price = original_price;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public float getDiscount() {
+		return discount;
+	}
+	public void setDiscount(float discount) {
+		this.discount = discount;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPhonme() {
+		return phonme;
+	}
+	public void setPhonme(int phonme) {
+		this.phonme = phonme;
+	}
+	public String getBussiness() {
+		return bussiness;
+	}
+	public void setBussiness(String bussiness) {
+		this.bussiness = bussiness;
+	}
+	public Date getPublish_date() {
+		return publish_date;
+	}
+	public void setPublish_date(Date publish_date) {
+		this.publish_date = publish_date;
+	}
+	public String getKeywords() {
+		return keywords;
+	}
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+	public String getPartner() {
+		return partner;
+	}
+	public void setPartner(String partner) {
+		this.partner = partner;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getFavicon() {
+		return favicon;
+	}
+	public void setFavicon(String favicon) {
+		this.favicon = favicon;
+	}
+	public float getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+	public float getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+	public int getId() {
+		return id;
+	}	
+	
+}
